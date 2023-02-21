2f507447ddb9066c014c89cf2246a3a863b9276e
harinder24 <hss022002@gmail.com>
2023-02-21 14:22:54 -0800

    images and quote added

---
+++ app/src/main/java/com/example/affirmation_app_kotlin_compose/data/AffirmationData.kt
@@ -0,0 +1,8 @@
+package com.example.affirmation_app_kotlin_compose.data
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class AffirmationData(@StringRes val quote: Int, @DrawableRes val pic: Int){
+
+}
---
+++ app/src/main/java/com/example/affirmation_app_kotlin_compose/data/AffirmationList.kt
@@ -0,0 +1,21 @@
+package com.example.affirmation_app_kotlin_compose.data
+
+import com.example.affirmation_app_kotlin_compose.R
+
+class AffirmationList() {
+    fun getData(): List<AffirmationData>{
+
+            return listOf<AffirmationData>(
+                AffirmationData(R.string.affirmation1, R.drawable.image1),
+                AffirmationData(R.string.affirmation2, R.drawable.image2),
+                AffirmationData(R.string.affirmation3, R.drawable.image3),
+                AffirmationData(R.string.affirmation4, R.drawable.image4),
+                AffirmationData(R.string.affirmation5, R.drawable.image5),
+                AffirmationData(R.string.affirmation6, R.drawable.image6),
+                AffirmationData(R.string.affirmation7, R.drawable.image7),
+                AffirmationData(R.string.affirmation8, R.drawable.image8),
+                AffirmationData(R.string.affirmation9, R.drawable.image9),
+                AffirmationData(R.string.affirmation10, R.drawable.image10))
+
+    }
+}
