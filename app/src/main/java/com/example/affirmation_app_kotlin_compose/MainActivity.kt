59d79d36681f031af9c77fb8f4e919a8ac9c4680
harinder24 <hss022002@gmail.com>
2023-02-21 15:02:26 -0800

    code finished

--- app/src/main/java/com/example/affirmation_app_kotlin_compose/MainActivity.kt
+++ app/src/main/java/com/example/affirmation_app_kotlin_compose/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.affirmation_app_kotlin_compose
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmation_app_kotlin_compose.data.AffirmationData
+import com.example.affirmation_app_kotlin_compose.data.AffirmationList
 import com.example.affirmation_app_kotlin_compose.ui.theme.AffirmationAppKotlin_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +30,45 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AffirmationAppKotlin_ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+               AffirmationApp()
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    AffirmationAppKotlin_ComposeTheme {
-        Greeting("Android")
+fun AffirmationApp(){
+
+    AffirmationList(AffirmationList.getData())
+}
+
+@Composable
+fun AffirmationList(listOfAffirmation: List<AffirmationData>){
+   LazyColumn{
+       items(listOfAffirmation){ item ->  
+       LoadUI(quote = item.quote, pic = item.pic)
+           
+       }
+   }
+}
+
+@Composable
+fun LoadUI(@StringRes quote: Int, @DrawableRes pic: Int){
+    Card(modifier = Modifier.padding(all = 8.dp), elevation = 4.dp) {
+        Column {
+            Image(painter = painterResource(id = pic), contentDescription = stringResource(id = quote), modifier = Modifier
+                .fillMaxWidth()
+                .height(196.dp), contentScale = ContentScale.Crop)
+            Text(text = stringResource(id = quote),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.h6)
+        }
     }
 }
+
+
+
+
+
+
+
--- app/src/main/java/com/example/affirmation_app_kotlin_compose/data/AffirmationList.kt
+++ app/src/main/java/com/example/affirmation_app_kotlin_compose/data/AffirmationList.kt
@@ -18,4 +18,20 @@ class AffirmationList() {
                 AffirmationData(R.string.affirmation10, R.drawable.image10))
 
     }
+
+    companion object {
+        fun getData(): List<AffirmationData> {
+            return listOf<AffirmationData>(
+                AffirmationData(R.string.affirmation1, R.drawable.image1),
+                AffirmationData(R.string.affirmation2, R.drawable.image2),
+                AffirmationData(R.string.affirmation3, R.drawable.image3),
+                AffirmationData(R.string.affirmation4, R.drawable.image4),
+                AffirmationData(R.string.affirmation5, R.drawable.image5),
+                AffirmationData(R.string.affirmation6, R.drawable.image6),
+                AffirmationData(R.string.affirmation7, R.drawable.image7),
+                AffirmationData(R.string.affirmation8, R.drawable.image8),
+                AffirmationData(R.string.affirmation9, R.drawable.image9),
+                AffirmationData(R.string.affirmation10, R.drawable.image10))
+        }
+    }
 }
